d24f9b7468ad1f4683b9a037f9049d80b25ac280
Alana Abreu <149194023+N1nh4@users.noreply.github.com>
2025-10-01 17:17:40 -0300

    estrutura do model Atividade

--- src/main/java/com/example/organize/model/Atividade.java
+++ src/main/java/com/example/organize/model/Atividade.java
@@ -1,5 +1,33 @@
 package com.example.organize.model;
 
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+@Entity
 public class Atividade {
-    
+    private String nome;
+    private String descricao;
+    private String status;
+    private String obsercacao;
+
+    @OneToMany(mappedBy = "atividade", cascade = jakarta.persistence.CascadeType.ALL, fetch = jakarta.persistence.FetchType.LAZY)
+    private List<AtividadeColuna> colunas;
+
+    @ManyToOne(fetch = jakarta.persistence.FetchType.LAZY)
+    @JoinColumn(name = "id_usuario")
+    private Usuario usuario;
+
 }
