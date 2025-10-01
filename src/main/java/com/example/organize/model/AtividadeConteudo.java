28c3e98e00f5edce57d7dbcdd10cdd1e421051c9
Alana Abreu <149194023+N1nh4@users.noreply.github.com>
2025-10-01 17:09:12 -0300

    estrutura do model AtividadeConteudo

--- src/main/java/com/example/organize/model/AtividadeConteudo.java
+++ src/main/java/com/example/organize/model/AtividadeConteudo.java
@@ -1,5 +1,34 @@
 package com.example.organize.model;
 
+import java.util.UUID;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+@Entity
 public class AtividadeConteudo {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+    private String conteudo;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_atividade_coluna")
+    private AtividadeColuna atividadeColuna;
+
+
 }
