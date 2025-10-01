60556b6339efa713263a8acf111c9e8842357ddb
Alana Abreu <149194023+N1nh4@users.noreply.github.com>
2025-10-01 17:12:06 -0300

    estrutura do model AtividadeColuna

--- src/main/java/com/example/organize/model/AtividadeColuna.java
+++ src/main/java/com/example/organize/model/AtividadeColuna.java
@@ -1,5 +1,30 @@
 package com.example.organize.model;
 
+import java.util.UUID;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+@Entity
 public class AtividadeColuna {
-    
+    @Id
+    @GeneratedValue(strategy = jakarta.persistence.GenerationType.AUTO)
+    private UUID id;
+    private String nome;
+
+    @ManyToOne(fetch = jakarta.persistence.FetchType.LAZY)
+    @JoinColumn(name = "id_atividade")
+    private Atividade atividade;
 }
