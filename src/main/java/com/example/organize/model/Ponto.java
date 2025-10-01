3557b0e550af1e0b204302dd22b5f5c97b3f5481
Alana Abreu <149194023+N1nh4@users.noreply.github.com>
2025-10-01 17:01:47 -0300

    estrutura do model Ponto

--- src/main/java/com/example/organize/model/Ponto.java
+++ src/main/java/com/example/organize/model/Ponto.java
@@ -1,5 +1,38 @@
 package com.example.organize.model;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+@Entity
 public class Ponto {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+    private LocalDateTime pontoInicial;
+    private LocalDateTime pontoFinal;
+    private Duration meta;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_usuario")
+    private Usuario usuario;
+
+
 }
